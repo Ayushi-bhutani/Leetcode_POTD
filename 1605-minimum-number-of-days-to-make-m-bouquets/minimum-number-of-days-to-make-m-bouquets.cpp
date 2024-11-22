9903ba86eb70e9db3993507760ce12f23c0d9afa
Ayushi-bhutani <119058732+Ayushi-bhutani@users.noreply.github.com>
2024-11-22 15:48:53 +0530

    Time: 17 ms (88.60%) | Memory: 69.1 MB (91.56%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -2,22 +2,23 @@ class Solution {
 public:
     
     bool possible(vector<int> &arr, int day, int m, int k) {
-        int n = arr.size(); //size of the array
-            int cnt = 0;
-                int noOfB = 0;
-                    // count the number of bouquets:
-                        for (int i = 0; i < n; i++) {
-                                if (arr[i] <= day) {
-                                            cnt++;
-                                                    }
-                                                            else {
-                                                                        noOfB += (cnt / k);
-                                                                                    cnt = 0;
-                                                                                            }
-                                                                                                }
-                                                                                                    noOfB += (cnt / k);
-                                                                                                        return noOfB >= m;
-                                                                                                        }
+        int n = arr.size();
+        int cnt =0;
+        int noOfB =0;
+        for(int i=0; i<n; i++){
+            if(arr[i] <= day){
+                cnt++;
+
+            }else{
+                noOfB += cnt/k;
+                cnt = 0;
+            }
+
+
+        }
+        noOfB +=(cnt/k);
+        return noOfB >= m;
+                                                                           }
     int minDays(vector<int>& arr, int m, int k) {
          long long val = m * 1ll * k * 1ll;
              int n = arr.size(); //size of the array
