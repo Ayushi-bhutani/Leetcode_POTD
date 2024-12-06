576761a45b10295071fb8db823fe0763e2ffbc81
Ayushi-bhutani <119058732+Ayushi-bhutani@users.noreply.github.com>
2024-12-06 23:34:56 +0530

    Time: 189 ms (56.49%) | Memory: 178 MB (47.98%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     int maxCount(vector<int>& arr, int n, int maxSum) {
-        vector<int> ans;
-        for(int i=1; i<=n; i++){
-            if( find(arr.begin(), arr.end(), i)==arr.end()){
-                ans.push_back(i);
-            }
-
-
-        }
-        sort(ans.begin(), ans.end());
-        int sum=0;
+        unordered_set<int>arrSet(arr.begin(), arr.end());
+        int sum =0;
         int count =0;
-        for(int i=0; i<ans.size(); i++){
-            if(sum + ans[i] <= maxSum){
-                sum += ans[i];
-                count++;
-            }else{
-                break;
+        for(int i=1; i<=n ; i++){
+            if(arrSet.find(i) == arrSet.end()){
+                if(i+sum <= maxSum){
+                    sum += i;
+                    count++;
+                }else{
+                    break;
+                }
             }
-            
         }
         return count;
          
